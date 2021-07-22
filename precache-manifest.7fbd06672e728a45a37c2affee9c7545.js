4b4d1bc00bef36c97627a42b72487feae829bd49
petarmihaylov <petarmihaylov@users.noreply.github.com>
2021-07-22 04:36:13 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.7fbd06672e728a45a37c2affee9c7545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cd8fc74fbb3b13ccfcf2c71870c891",
+    "url": "https://petarmihaylov.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "0c551946fbba045f8821",
+    "url": "https://petarmihaylov.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "57afc37490194ff8caca",
+    "url": "https://petarmihaylov.github.io/task-management-frontend/static/js/2.89d9af29.chunk.js"
+  },
+  {
+    "revision": "0c551946fbba045f8821",
+    "url": "https://petarmihaylov.github.io/task-management-frontend/static/js/main.022f8bfd.chunk.js"
+  },
+  {
+    "revision": "81d00899c460d85db6d5",
+    "url": "https://petarmihaylov.github.io/task-management-frontend/static/js/runtime~main.9119b0fc.js"
+  }
+]);
